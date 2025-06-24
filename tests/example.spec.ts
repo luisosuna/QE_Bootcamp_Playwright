9da6d424e6666d04839ba74e43029ed4c3a505db
Luis <luis.osuna@unosquare.com>
2025-06-24 17:33:12 -0600

    Locators Exercise

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -1,18 +1,8 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
-
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+test('login with valid credentials using getByLabel and getByRole', async ({ page }) => {
+  await page.goto('https://www.saucedemo.com');
+  await page.getByLabel('Username').fill('standard_user');
+  await page.getByLabel('Password').fill('secret_sauce');
+  await page.getByRole('button', { name: 'Login' }).click();
 });
